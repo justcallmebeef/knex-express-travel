a2f975fbfce874189a4efe32f92126d94931371f
justcallmebeef <bethanymitch10@gmail.com>
2018-11-29 11:35:24 -0700

    added get by id route in queries and added an app.get in index.js

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/:id', (req, res) => {
+    id = req.params.id
+    queries.getById(id).then(locations => {
+        res.send(locations)
+    })
+})
+
 app.use((req, res, next) => {
     res.status(404).json({ error: { message: 'data not found' }})
   })
--- queries.js
+++ queries.js
@@ -3,5 +3,8 @@ const database = require('./database-connection')
 module.exports = {
     listAll() {
         return database('locations')
-    }
+    }, 
+    getById(id) {
+        return database('locations').where({id: id}).first()
+    } 
 }
